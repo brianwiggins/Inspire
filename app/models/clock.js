83b4005c951563a1957f24975ced6891918f8fe9
brianwiggins <peppermint.butl3r@gmail.com>
2020-02-15 21:27:35 -0700

    got time model set up

--- app/models/clock.js
+++ app/models/clock.js
@@ -1,6 +1,16 @@
 export default class Clock {
   constructor(data) {
-    this.time = data.time;
-    this.date = data.date;
+    this.date = this.getDate()
+    this.time = this.getTime();
+  }
+  getDate() {
+    let newDate = new Date();
+    let date = newDate.getMonth() + ' ' + newDate.getDate() + ', ' + newDate.getFullYear();
+    return date;
+  }
+  getTime() {
+    let newTime = new Date();
+    let time = newTime.getHours() + ":" + newTime.getMinutes();
+    return time;
   }
 }
